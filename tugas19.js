49d6104418b3f7302600e9e8b5db7407bc25f131
Arya Naufal <arya.naufal.andt@gmail.com>
2022-01-04 18:01:15 +0700

    TUGAS 19 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas19.js
@@ -0,0 +1,12 @@
+var kata = "semangat...";
+var bintang = "****"
+
+console.log(kata.repeat(5))
+
+console.log(bintang)
+console.log(bintang.repeat(5))
+console.log(bintang.repeat(9))
+console.log(bintang.repeat(13))
+console.log(bintang.repeat(9))
+console.log(bintang.repeat(5))
+console.log(bintang)
